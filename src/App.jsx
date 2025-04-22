8c37215d8a84aefee8274d566bc895bcd6da22ac
Kaz Alvis <kazalvis@Kazs-iMac.local>
2025-04-22 12:56:13 -0700

    cleanout old files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import HeroScreen from './pages/HeroScreen';
-import TVPlayer from './pages/TVPlayer';
 
 function App() {
   const [hasStarted, setHasStarted] = useState(false);
